5899eda98e7e11c02f58f7dedcff6d88fd2d547a
김성엽 <tjdduq1213@gmail.com>
2025-07-01 23:27:50 +0000

    minor chagne(meta data: title)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import './globals.css';
 import { Toaster } from "@/components/ui/toaster";
 
 export const metadata: Metadata = {
-  title: 'English Composition Analyzer AI',
+  title: '영작문 분석기 AI',
   description: 'AI 기반 영작문 분석기',
 };
 
